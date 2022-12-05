af3ac8d0b5a1c41677d7abe6a73c007f00156712
nicole moodley <nicolem@cognician.com>
2022-12-05 23:17:06 +0200

    Add Day 5

--- src/aoc_2022/core.clj
+++ src/aoc_2022/core.clj
@@ -1,5 +1,10 @@
 (ns aoc-2022.core
   (:require [clojure.string :as string]))
 
-(defn read-input [n]
-  (string/split-lines (slurp (str "resources/inputs/day" n ".txt"))))
+(defn read-input
+  ([n split?]
+   (if split?
+     (string/split-lines (slurp (str "resources/inputs/day" n ".txt")))
+     (slurp (str "resources/inputs/day" n ".txt"))))
+  ([n]
+   (read-input n true)))
---
+++ src/aoc_2022/day5.clj
@@ -0,0 +1,48 @@
+(ns day5
+  (:require
+   [aoc-2022.core :as a22.core]
+   [clojure.string :as string]))
+
+(def input (a22.core/read-input 5))
+
+(defn parse-line [s]
+  (if-let [instructions (re-seq #"\d+" s)]
+    (map parse-long instructions)
+    (map str (take-nth 4 (rest s)))))
+
+(defn read-stacks [input]
+  (let [stack-height
+        (first (keep-indexed #(when (string/starts-with? %2 " 1") %1) input))]
+    (->> (map parse-line (take stack-height input))
+         (apply map vector) ; transpose
+         (map #(remove string/blank? %))
+         (zipmap (range 1 (+ stack-height 2)))
+         (into (sorted-map)))))
+
+(defn read-instructions [input]
+  (->> (filter #(string/starts-with? % "m") input)
+       (map parse-line)))
+
+(defn move-crates [reverse? stacks instructions]
+  (let [[move-number A B] instructions
+        chosen-crates (take move-number (get stacks A))
+        chosen-crates (if reverse? (reverse chosen-crates) chosen-crates)]
+    (-> stacks
+        (update A (partial drop move-number))
+        (update B (partial concat chosen-crates)))))
+
+(defn move-all-crates [reverse? initial-stacks instructions]
+  (reduce (partial move-crates reverse?)
+   initial-stacks instructions))
+
+(defn part-one []
+  (->> (move-all-crates true (read-stacks input) (read-instructions input))
+       vals
+       (map first)
+       (apply str)))
+
+(defn part-two []
+  (->> (move-all-crates false (read-stacks input) (read-instructions input))
+       vals
+       (map first)
+       (apply str)))
